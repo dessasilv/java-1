373e0a45978c26efc79dc4f240f6856ed5663bf7
dessasilv <andressasilva1423ag@gmail.com>
2023-05-16 09:19:01 -0300

    .

--- myscript.js
+++ myscript.js
@@ -1,6 +1,15 @@
 var numero1 = parseInt(window.prompt ('escreva um número') * 1);
 var numero2 = parseInt(window.prompt ('escreva outro numero') * 1);
+var numero3 = parseInt(window.prompt ('escreva outro numero') * 1)
+
+var nome = window.prompt('seu nome: ');
+var salario = parseInt(window.prompt('seu salario: '));
+var aumento = salario * 0.10
+
+document.write('a media é:', Math.floor((numero1+numero2+numero3)/3), );
+
+document.write("seu nome é: ", nome, '; seu salario: ', aumento + salario)
 
 
-document.write('a media é:', ((numero1+numero2)/2) );
+
 
