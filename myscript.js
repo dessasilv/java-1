9227cebb842b8578067089eeb83547eba03b682f
dessasilv <andressasilva1423ag@gmail.com>
2023-05-16 08:55:18 -0300

    .

---
+++ myscript.js
@@ -0,0 +1,6 @@
+var numero1 = parseInt(window.prompt ('escreva um número') * 1);
+var numero2 = parseInt(window.prompt ('escreva outro numero') * 1);
+
+
+document.write('a media é:', ((numero1+numero2)/2) );
+
